426e572b383f0b357e0e6f930106f1e54b8bde1e
xia0 <cpt.acky@gmail.com>
2022-09-24 17:14:09 +0930

    test getting playlists

--- spotify-trivia/main.js
+++ spotify-trivia/main.js
@@ -165,6 +165,33 @@
         console.error(error);
         mainPlaceholder.innerHTML = errorTemplate(error.error);
       });
+
+    getPlayLists();
+  }
+
+  function getPlaylists() {
+    fetch('https://api.spotify.com/v1/me/playlists', {
+      headers: {
+        Authorization: 'Bearer ' + access_token,
+      },
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw await response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        //document.getElementById('login').style.display = 'none';
+        //document.getElementById('loggedin').style.display = 'unset';
+        //mainPlaceholder.innerHTML = userProfileTemplate(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        mainPlaceholder.innerHTML = errorTemplate(error.error);
+      });
   }
 
   function userProfileTemplate(data) {
